1b74513c6ec8e1447cde559ea4cd700fa2633bc4
Wesley Brown <wesleybrown792@u.boisestate.edu>
2021-02-17 15:26:50 -0700

    added anything new from today

--- Dao.php
+++ Dao.php
@@ -1,2 +1,49 @@
 <?php
-//this is where we will set up the php server functionality as well as set up connections to the sql server
+    class Dao{
+        private $host = "us-cdbr-east-03.cleardb.com";
+        private $db = "heroku_a8ca269478ee9f2";
+        private $user = "b09ced2ae4c4e0";
+        private $pass = "ad38ea62";
+
+
+        public function getConnection() {
+            try{
+                $conn = new PDO ("mysql:host={$this->host};dbname={$this->db}", $this->user,$this->pass);
+                return $conn;
+            }catch (Exception $e){
+                echo print_r($e,1);
+                exit;
+            }
+        }
+
+        /* public function addUser($user, $pass, $acc){
+            $conn = $this->getConnection();
+            $saveQ = "insert into user (username,password,access) values (:user,:pass,:acc);";
+            $q = $conn->prepare($saveQ);
+            $q->bindParam(":user",$user);
+            $q->bindParam(":pass",$pass);
+            $q->bindParam(":acc",$acc);
+            $q->execute();
+        } */
+        
+        /* public function userExists($user, $pass){
+            $conn = $this->getConnection();
+            $stmt =  $conn->prepare("select * from user where username = :user and password = :pass;");
+            $stmt->bindParam(":user",$user,PDO::PARAM_STR);
+            $stmt->bindParam(":pass",$pass,PDO::PARAM_STR);
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            if(is_null($user)||$user==""){
+                return false;
+            }else{
+                if(count($result)==1){
+                    return true;
+                }else{
+                    return false;
+                }
+            }
+        
+        } */
+    }
+?>
