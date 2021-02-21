55aa858009f19c7ced4dda46add8dc06dd256244
TalkTomeG00se <47369415+TalkTomeG00se@users.noreply.github.com>
2021-02-20 22:46:53 -0700

    Implements #21 and #15
    Customizing home page via css for task #21, story #15

--- index.php
+++ index.php
@@ -2,10 +2,16 @@
     //this is the homepage 
     require_once "components/nav.php";
 ?>
-<html>
     <head>
+        <link rel="stylesheet" href="home.css">
     </head>
+    <h1>Welcome To SignUp Dummy</h1>
 
+    <div id="content">
+        <p id="about">
+        Bacon ipsum dolor amet cupim frankfurter non, short ribs lorem eu spare ribs salami tail magna porchetta turkey. Occaecat est shank filet mignon pork duis drumstick spare ribs meatloaf meatball. Pork belly porchetta bresaola tempor tri-tip kielbasa chuck short loin consequat cupim ball tip pork chop strip steak. Sausage shankle brisket deserunt landjaeger proident porchetta ham hock ipsum cupidatat esse.
+        </p>
+    </div>
     <body>
        
 
