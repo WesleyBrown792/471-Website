69986711bc65b5e34d94784c68d92d442b1a5b16
Chloe Johnson <chloejohnson@u.boisestate.edu>
2021-03-06 12:15:10 -0700

    Added createevent.php

---
+++ createevent.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+?>
+
+<html>
+
+<head>
+    <script
+    src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <title>Create Event</title>
+    <link rel="stylesheet" href="css/signin.css">
+    <?php
+    include "components/head.php";
+    ?>
+</head>
+
+<body>
+    <div>
+        <?php
+        include "components/nav.php";
+        ?>
+        <h1>Create Event</h1>
+        <form method="POST" action="login.php" onSubmit="return Empty()">
+            <div class="form-group">
+                <input type="text" placeholder="Event Title *" value="" name="eventName" id="eventName"/>
+            </div>
+            <!-- I think dates and times should be side by side.
+            I also think dates to have a popup calander and times should have
+            drop down selector.
+            -->
+            <div class="form-group">
+                <input type="date" name="startDate" id="startDate" placeholder="Start Date *" value="" />
+            </div>
+            <div class="form-group">
+            <input type="time" name="startTime" id="startTime" placeholder="Start Time *" value="" />
+            </div>
+            <div class="form-group">
+            <input type="date" name="endDate" id="endDate" placeholder="End Date *" value="" />
+            </div>
+            <div class="form-group">
+            <input type="time" name="endTime" id="endTime" placeholder="End Time *" value="" />
+            </div>
+            <div class="form-group">
+                <input type="submit" value="Create Event" />
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
