7563b2a73886cc7e55094a408d3a4ca568b52d25
TalkTomeG00se <47369415+TalkTomeG00se@users.noreply.github.com>
2021-03-05 21:45:39 -0700

    Update nav.php

--- components/nav.php
+++ components/nav.php
@@ -21,7 +21,7 @@ session_start();
                 <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
                     <div class="navbar-nav">
                     <?php
-                    if($_SESSION["authenticated"] == TRUE){ // checks if user logged in...not working
+                    if($_SESSION["authenticated"] === TRUE){ 
 
                         echo "<a class='nav-item nav-link' href='logout.php'>Thinks Session is Set</a>"; // logged in, show logout
                     } else {
