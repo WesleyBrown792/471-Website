49a66143bed3602726b8e54bae0cc052faf6b7fa
TalkTomeG00se <47369415+TalkTomeG00se@users.noreply.github.com>
2021-03-06 22:01:56 -0700

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -5,10 +5,21 @@ session_start();
 <html>
 
     <head>
-  
+    <script
+    src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <title>Create Event</title>
+    <link rel="stylesheet" href="css/signin.css">
+    <?php
+    include "components/head.php";
+    ?>
     </head>
 
     <body>
+        <div>
+            <?php
+                include "components/nav.php";
+            ?>
+        </div>
     
     </body>
 
