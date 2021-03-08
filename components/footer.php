ebd63cf56bd98b9349d6f900ef540a086d26f70e
Nicholas Prussen <59841031+nicholasprussen@users.noreply.github.com>
2021-03-07 18:04:55 -0700

    changed style of footer to follow sticky bottom

--- components/footer.php
+++ components/footer.php
@@ -1,15 +1,16 @@
 <html>
 
-<body>
-    <div>
-       <footer>
-            <p>Authors: Wesley Brown, Nickolas lastnameredacted, Chloe Johnson, Talktomegoose lastnameredacted</p>
-            <p>Published: 3/6/2021</p>
-            <p>This site was made as a group project for Boise State's CS 471 Software Engineering class.</p>
-            <p>
-       </footer>
-    </div>
+<head>
+    <link rel="stylesheet" href="css/footer.css">
+</head>
 
-</body>
+
+<footer class="footer mt-auto py-3 bg-dark">
+    <p>Authors: Wesley Brown, Nicholas lastnameredacted, Chloe Johnson, Talktomegoose lastnameredacted</p>
+    <p>Published: 3/6/2021</p>
+    <p>This site was made as a group project for Boise State's CS 471 Software Engineering class.</p>
+    <p>
+</footer>
+
 
 </html>
