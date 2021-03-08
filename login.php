b00bde9a7cb7ec0d3014205e341eeed64e8dc8ff
Wesley Brown <wesleybrown792@u.boisestate.edu>
2021-03-07 20:33:05 -0700

    fix login

--- login.php
+++ login.php
@@ -13,26 +13,14 @@ if ($dao->userExists($email, $newpass)) {
         $_SESSION['authenticated'] = true;
         $_SESSION['isLoggedIn'] = "yes";
         header("Location: welcome.php");
-<<<<<<< Updated upstream
-        exit();
-=======
->>>>>>> Stashed changes
     }else{
         $_SESSION['authenticated'] = true;
         $_SESSION['isLoggedIn'] = "yes";
         header("Location: welcome.php");
-<<<<<<< Updated upstream
-        exit();
-=======
->>>>>>> Stashed changes
     }
 } else {
     $_SESSION['authenticated'] = false;
     $_SESSION['email'] = $email;
     $_SESSION['isLoggedIn'] = "no";
     header("Location: signin.php");
-<<<<<<< Updated upstream
-    exit();
-=======
->>>>>>> Stashed changes
 }
