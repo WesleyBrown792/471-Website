d3ac0147c2d2a01f659cd61fa80f7a1953e64489
TalkTomeG00se <47369415+TalkTomeG00se@users.noreply.github.com>
2021-04-13 21:32:53 -0600

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -24,11 +24,11 @@ if($_SESSION['authenticated']){
     <div class="container-xl p-0 welcome-page-container">
 
         <div class="welcome-page-button-container">
-            <button type="button" onclick="createvent.php">Create an Event</button>
+            <button type="button" onclick="location.href='createvent.php'">Create an Event</button>
             <br>
-            <button type="button" onclick="eventlist.php">See Events</button>
+            <button type="button" onclick="location.href='eventlist.php'">See Events</button>
             <br>
-            <button type="button" onclick="modifyprof.php">Modify Profile</button>
+            <button type="button" onclick="location.href='modifyprof.php'">Modify Profile</button>
         </div>
 
 
