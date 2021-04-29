3daa5be095de48c9241eccc37cdf7af060b89666
TalkTomeG00se <47369415+TalkTomeG00se@users.noreply.github.com>
2021-04-28 21:59:07 -0600

    Update addquestion.php

--- addquestion.php
+++ addquestion.php
@@ -1,15 +1,14 @@
 <?php
     session_start();
     require_once 'Dao.php';
-
     $question = $_GET['question'];
     $ans = "No answer yet";
     $email = "defaultemail";
-
     $conn = $this->getConnection();
     $q = $conn->prepare("INSERT INTO questions (questionEmail, questionAsk, questionAnswer) VALUES (?,?,?)");
     $q->bind_param("sss", $email, $question, $ans);
     $q->execute();
-
+    $q->close();
+    $conn->close();
     header("location:faq.php");
     exit();
